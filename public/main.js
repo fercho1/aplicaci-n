b3125587cf24f30838f76270f2a30e39dcee9bad
Fernando <ferchonunez1@hotmail.com>
2020-11-25 15:28:06 -0500

    Ang

--- public/main.js
+++ public/main.js
@@ -3032,9 +3032,13 @@ __webpack_require__.r(__webpack_exports__);
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+/* export const environment = {
+  production: false,
+  base_url: 'http://localhost:3000'
+}; */
 const environment = {
-    production: false,
-    base_url: 'http://localhost:3000'
+    production: true,
+    base_url: 'https://appdia.herokuapp.com'
 };
 /*
  * For easier debugging in development mode, you can import the following file
